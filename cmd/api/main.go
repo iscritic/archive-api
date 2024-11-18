350dab70b542a1a228d9000fb2fc970cca89b8f3
arthkim <k1m4rti@gmail.com>
2024-11-18 19:55:54 +0500

    add coms

--- cmd/api/main.go
+++ cmd/api/main.go
@@ -1,12 +1,13 @@
 package main
 
 import (
-	"fmt"
+	"github.com/gin-gonic/gin"
 	"github.com/iscritic/archive-api/internal/config"
 	"github.com/iscritic/archive-api/internal/server"
 	"github.com/joho/godotenv"
 	"log"
 	"log/slog"
+	"os"
 )
 
 func main() {
@@ -17,14 +18,20 @@ func main() {
 	}
 
 	cfg := config.MustLoad()
+	slog.Info("Config is set...")
 
-	fmt.Println(cfg)
+	gin.SetMode(gin.ReleaseMode)
 	srv := server.NewServer(cfg)
 
-	slog.Info("Starting API server...")
+	slog.Info("Starting API server",
+		slog.String("address", cfg.HTTPServer.Address),
+		slog.Int("port", cfg.HTTPServer.Port),
+	)
+
 	err = srv.Run()
 	if err != nil {
-		log.Fatal(err)
+		slog.Error("Failed to run server", slog.String("error", err.Error()))
+		os.Exit(1)
 	}
 
 }
